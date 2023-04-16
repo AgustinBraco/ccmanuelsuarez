e06a90c617b90c30f2c88a011be8640e5eeb8bab
AgustinBraco <bracoagustin@gmail.com>
2023-04-16 02:31:54 -0300

    testing-images-links

--- scripts.js
+++ scripts.js
@@ -9,3 +9,14 @@ open.addEventListener("click", () => {
 close.addEventListener("click", () => {
   nav.classList.remove("open-navbar")
 });
+
+import PhotoSwipeLightbox from '/photoswipe/photoswipe-lightbox.esm.js';
+import PhotoSwipe from '/photoswipe/photoswipe.esm.js';
+
+const lightbox = new PhotoSwipeLightbox({
+  gallery: '#gallery--no-dynamic-import',
+  children: 'a',
+  pswpModule: PhotoSwipe
+});
+
+lightbox.init();
