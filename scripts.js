9c5a8fe36b44dfef3c8e023bdb4611378059ac52
AgustinBraco <bracoagustin@gmail.com>
2023-04-18 22:17:28 -0300

    some-changes-all-done

--- scripts.js
+++ scripts.js
@@ -2,6 +2,7 @@ const nav = document.querySelector (".nav-bar");
 const open = document.querySelector(".menu-mobile");
 const close = document.querySelector(".close-menu");
 const banner = document.querySelector(".banner-image");
+const arrow = document.querySelector(".scroll-arrow-container");
 
 open.addEventListener("click", () => {
   nav.classList.add("open-navbar")
@@ -15,4 +16,5 @@ window.addEventListener("scroll", () => {
   let scroll = window.scrollY
   banner.style.top = scroll * 0.5 + "px";
   banner.style.webkitFilter = `blur(${scroll * 0.005}px)`;
+  arrow.style.bottom = scroll * 1 + "px";
 });
